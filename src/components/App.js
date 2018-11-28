f78d3a93d6adfd14e43ce327835aa26143470178
evertontatu <everton.boni@adena.com.br>
2018-11-27 22:14:55 -0200

    Add materialize and sass

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../App.css';
+import {Button, Icon, Card, Row, Col} from 'react-materialize';
+import 'materialize-css/dist/css/materialize.min.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Button waves='light'>
+          <Icon>thumb_up</Icon>
+        </Button>
+
+        <Button waves='light'>EDIT ME<Icon left>save</Icon></Button>
       </div>
     );
   }
