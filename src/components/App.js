2b8e06aa3d990ce8839cec57c190bcac1cbb37b5
evertontatu <everton.boni@adena.com.br>
2018-11-28 10:19:18 -0200

    init structure html

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
 import '../App.css';
-import {Button, Icon, Card, Row, Col} from 'react-materialize';
 import 'materialize-css/dist/css/materialize.min.css';
+import '../styles/index.sass';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Button waves='light'>
-          <Icon>thumb_up</Icon>
-        </Button>
 
-        <Button waves='light'>EDIT ME<Icon left>save</Icon></Button>
+        <nav>
+          <div class="nav-wrapper">
+            <a href="#" class="brand-logo">Leitura</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+              <li><a href="sass.html">Home</a></li>
+            </ul>
+          </div>
+        </nav>
+
+        <div class="input-field col s12">
+          <select>
+            <option value="" disabled selected>Choose your option</option>
+            <option value="1">Option 1</option>
+            <option value="2">Option 2</option>
+            <option value="3">Option 3</option>
+          </select>
+          <label>Materialize Select</label>
+        </div>
+
+      
       </div>
     );
   }
